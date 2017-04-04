dabdbc9a16e9e6e366ecc3e81369a97233181b8a
Yashasvi-Sriram <yash.3997@gmail.com>
2017-04-05 03:25:19 +0530

    implements : SocketStation class                 (Also implements SocketStation class in python as a counterpart)         1.Reliably transfers data in form of blocks         2.Implements (block)send, (block)receive features         3.Provides Block abstraction                 ...Block is the entire string which you give as param                    to send() (in java or python)                 ...When other end uses receive() method (in python)                    it recives this string only, even if there is extra string remaining                    in the buffer afterwards                 ...Similarly receive() (in java) will only pick up the string given to                    send() (in python) as param, 		   even if the buffer has more string afterwards         4.block_send() returns true if success, false otherwise           block_receive() retuns the string received if success, null otherwise         5.Provides a little error handling using r_body_limit 	6.Improves connect() and close() using isClosed() method

--- app/src/main/java/com/withjarvis/sayit/SocketStation/SocketStation.java
+++ app/src/main/java/com/withjarvis/sayit/SocketStation/SocketStation.java
@@ -1,8 +1,8 @@
-package com.withjarvis.sayit.SocketStation;
+//package com.withjarvis.sayit.SocketStation;
 
-import android.util.Log;
+//import android.util.Log;
 
-import com.withjarvis.sayit.JLog.JLog;
+//import com.withjarvis.sayit.JLog.JLog;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -13,9 +13,19 @@ import java.net.Socket;
 
 /**
  * Wrapper for socket formation and communication
- * */
+ */
 public class SocketStation {
-    private String serverAddress; // Must not be an empty string
+    // Must be same for client and server
+    // Cannot be a digit
+    // Must not be an empty string
+    // Cannot be empty string or char
+    private static final char head_body_delimiter = '\n';
+
+    // Max number of digits in length of body received
+    // Must be same for client and server
+    private static final int r_body_limit = 10;
+
+    private String serverAddress; // Must not be empty
     private int port;  // Port range 0 - 65536
 
     private Socket socket;
@@ -29,7 +39,7 @@ public class SocketStation {
 
     /**
      * Calling this method will close the previously connected socket if any
-     * */
+     */
     public void connect(String serverAddress, int port) {
         this.serverAddress = serverAddress;
         this.port = port;
@@ -43,7 +53,7 @@ public class SocketStation {
             // Keep track of it's input and output streams
             this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            Log.i(JLog.TAG, "Connection Established to " + this.socket.getRemoteSocketAddress());
+            // Log.i(JLog.TAG, "Connection Established to " + this.socket.getRemoteSocketAddress());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -51,12 +61,19 @@ public class SocketStation {
 
     /**
      * Takes the param and writes it and then flushes it using the BufferedOutputStream
-     * */
-    public boolean send(String data) {
+     * Packet Format = body_length(delimiter)body
+     */
+    public boolean send(String body) {
         try {
-            this.writer.write(data);
+            int data_length = body.length();
+            StringBuilder packet_builder = new StringBuilder();
+            packet_builder.append(String.valueOf(data_length))
+                    .append(SocketStation.head_body_delimiter)
+                    .append(body);
+            System.out.println(packet_builder.toString());
+            this.writer.write(packet_builder.toString());
             this.writer.flush();
-            Log.i(JLog.TAG, "Sent data : " + data + " to " + this.serverAddress + " on port " + this.port);
+            // Log.i(JLog.TAG, "Sent data : to " + this.serverAddress + " on port " + this.port);
             return true;
         } catch (IOException e) {
             e.printStackTrace();
@@ -65,18 +82,42 @@ public class SocketStation {
     }
 
     /**
-     * On the InputStream reads a LINE!!
-     * If the InputStream has data without an new line (\n or \r or \n\r)
-     *      then it blocks until such a new line char is received in InputStream
-     * But if the socket closes in such case, then it reads that remaining data left on buffer
-     * */
+     * Expected Packet Format = body_length(delimiter)body'\n'
+     * Returns body
+     */
     public String receive() {
         try {
-            String data = this.reader.readLine();
-            if (data != null) {
-                Log.i(JLog.TAG, "Received data : " + data + " from " + this.serverAddress + " on port " + this.port);
+            int no_digits = 0;
+            StringBuilder body_length_builder = new StringBuilder();
+            // Handles (nothing)(delimiter) case (not req if format is proper)
+            body_length_builder.append('0');
+            // Finds body length
+            char letter;
+            while (true) {
+                letter = (char) this.reader.read();
+                if (letter == SocketStation.head_body_delimiter)
+                    break;
+                body_length_builder.append(letter);
+                // Limits body size
+                no_digits++;
+                if (no_digits > SocketStation.r_body_limit)
+                    return null;
             }
-            return data;
+            int body_length = Integer.parseInt(body_length_builder.toString());
+            // Extracts the body
+            StringBuilder body_builder = new StringBuilder();
+            String chunk;
+            while (body_length > 0) {
+                // This will at most read total body
+                chunk = this.reader.readLine();
+                body_builder.append(chunk).append('\n');
+                // + 1 is for the extra \n which is silently removed in readline()
+                body_length -= (chunk.length() + 1);
+            }
+            String body_with_newline = body_builder.toString();
+            // Removes the last newline appended for reading purpose
+            return body_with_newline.substring(0, body_with_newline.length() - 1);
+            // Log.i(JLog.TAG, "Received data : from " + this.serverAddress + " on port " + this.port);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -85,13 +126,13 @@ public class SocketStation {
 
     /**
      * Closes the socket if not already closed
-     * */
+     */
     public void close() {
         try {
             // If the socket is not already closed close it
             if (!this.socket.isClosed())
                 this.socket.close();
-            Log.i(JLog.TAG, "Connection closed properly with " + this.socket.getRemoteSocketAddress());
+            // Log.i(JLog.TAG, "Connection closed properly with " + this.socket.getRemoteSocketAddress());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -101,7 +142,7 @@ public class SocketStation {
      * If the GC collects this instance release the connection by closing the socket
      * DO NOT depend on this !!
      * Close the sockets by yourself religiously.
-     * */
+     */
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
@@ -110,10 +151,17 @@ public class SocketStation {
 
 //    public static void main(String[] args) {
 //        SocketStation a = new SocketStation("localhost", 8036);
-//        a.send("1");
-//        a.send("2");
+//        a.send("jklsdkfa;klufieif4987&*&(*&))&$#&_()_)(78~");
+//        a.send("");
+//        a.send("#&_()_)(78~");
+//        a.send("\n");
+//        a.send("This is second one and should be interpreted like wise ...");
 //        System.out.println(a.receive());
-//        a.close();
+//        System.out.println(a.receive());
+//        System.out.println(a.receive());
+//        System.out.println(a.receive());
+//        System.out.println(a.receive());
+//        System.out.println(a.receive());
 //    }
 
 }
